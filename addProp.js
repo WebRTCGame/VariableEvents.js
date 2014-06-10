629176521a61e9509ecda828c5e8cac32b6d959e
WebRTCGame <ge.unitel@gmail.com>
2014-06-10 07:52:49 -0500

    Update addProp.js

--- addProp.js
+++ addProp.js
@@ -1,24 +1,28 @@
-function addProp(obj, propname, defaultValue, getCB, setCB, modGet, modSet) {
-    var i = "_" + propname.toString(),
-        f = 'function';
-    obj[i] = {
-        val: defaultValue,
-        g: getCB,
-        s: setCB,
-        mg: modGet,
-        ms: modSet
-    };
-    Object.defineProperty(obj, propname, {
-        get: function () {
-            f === typeof obj[i].g && obj[i].g(obj[i].val);
-            return (f === typeof obj[i].mg) ? obj[i].mg(obj[i].val) : obj[i].val;
-        },
-        set: function (value) {
-            obj[i].val = (f === typeof obj[i].ms) ? obj[i].ms(value) : value;
-            f === typeof obj[i].s && obj[i].s(obj[i].val);
-        },
-        enumerable: true,
-        configurable: true
-    });
-    return this;
-};
+var addProp = (function () {
+
+    return {
+        add: function (obj, propname, defaultValue, getCB, setCB, modGet, modSet) {
+            var i = "_" + propname.toString(),
+                f = 'function';
+            obj[i] = {
+                val: defaultValue,
+                g: getCB,
+                s: setCB,
+                mg: modGet,
+                ms: modSet
+            };
+            Object.defineProperty(obj, propname, {
+                get: function () {
+                    f === typeof obj[i].g && obj[i].g(obj[i].val);
+                    return (f === typeof obj[i].mg) ? obj[i].mg(obj[i].val) : obj[i].val;
+                },
+                set: function (value) {
+                    obj[i].val = (f === typeof obj[i].ms) ? obj[i].ms(value) : value;
+                    f === typeof obj[i].s && obj[i].s(obj[i].val);
+                },
+                enumerable: true,
+                configurable: true
+            });
+        }
+    }
+})();
